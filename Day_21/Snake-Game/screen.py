f3ba34d5a38690325e57c6cadf6bc1b76ce9bc20
Adam Bayraktar <adambayraktar@gmail.com>
2023-01-11 21:05:20 +0100

    Add screen class file of Day 21 project - Snake Game

---
+++ Day_21/Snake-Game/screen.py
@@ -0,0 +1,31 @@
+from turtle import Screen
+
+
+class SnakeScreen:
+
+    def __init__(self):
+        self.screen = self.start()
+        self.__points = 0
+
+
+    def start(self):
+        screen = Screen()
+        screen.title("Classic Snake Game")
+        screen.listen()
+        screen.setup(width=620, height=620)
+        screen.tracer(0)
+        screen.bgcolor("black")
+        return screen
+
+    def update(self):
+        self.screen.update()
+
+    def bye(self):
+        self.screen.bye()
+
+    @property
+    def points(self):
+        return self.__points
+
+    def add_point(self):
+        self.__points += 1
