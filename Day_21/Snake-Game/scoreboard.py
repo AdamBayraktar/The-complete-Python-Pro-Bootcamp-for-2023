73b81592e97f99d63e023f8d03232894ac14bc3c
Adam Bayraktar <adambayraktar@gmail.com>
2023-01-16 15:50:30 +0100

    Add additional class HighScoreBoard in scoreboard file for Day 21 project - Snake Game

--- Day_21/Snake-Game/scoreboard.py
+++ Day_21/Snake-Game/scoreboard.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 
 class ScoreBoard(Turtle):
-
+    # shows actual score and can also show highscore
     def __init__(self):
         super().__init__()
         self.start()
@@ -11,13 +11,34 @@ class ScoreBoard(Turtle):
         self.color("white")
         self.ht()
         self.penup()
-        self.goto(0, 280)
-        self.write("Score: 0", align="center", font=('Arial', 18, 'normal'))
+        self.goto(-100, 280)
+        self.write(f"score: 0", align="center", font=('Arial', 18, 'normal'))
 
+        
+    # updates score
     def update_score(self, score):
         self.clear()
-        self.write(f"Score: {score}", align="center", font=('Arial', 18, 'normal'))
+        self.write(f"score: {score}", align="center", font=('Arial', 18, 'normal'))
 
+    # shows game over when snake hit wall or body 
     def game_over(self):
         self.goto(0,0)
         self.write("GAME OVER", align="center", font=('Arial', 28, 'normal'))
+
+    
+
+
+
+class HighScoreBoard(Turtle):
+    # shows actual score and can also show highscore
+    def __init__(self, highscore=0):
+        super().__init__()
+        self.__highscore = highscore
+        self.start()
+
+    def start(self):
+        self.color("white")
+        self.ht()
+        self.penup()
+        self.goto(100, 280)
+        self.write(f"highscore: {self.__highscore}", align="center", font=('Arial', 18, 'normal'))
